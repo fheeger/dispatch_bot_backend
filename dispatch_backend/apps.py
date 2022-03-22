82d264b91cc46940aabc2b82d78b50402dcc3459
Felix Heeger <fheeger@posteo.de>
2022-03-22 22:40:49 +0100

    moved bot startup to ready hook

--- dispatch_backend/apps.py
+++ dispatch_backend/apps.py
@@ -1,5 +1,10 @@
+import subprocess
+
 from django.apps import AppConfig
 
 
 class BotConfig(AppConfig):
     name = 'dispatch_backend'
+
+    def ready(self):
+        subprocess.Popen(["python", "bot_script.py"])
