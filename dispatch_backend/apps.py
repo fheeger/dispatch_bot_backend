6f697e9e8b9047f8ae5b33333d2a919070017f3a
Felix Heeger <fheeger@posteo.de>
2022-03-22 23:05:00 +0100

    testing for startup hook

--- dispatch_backend/apps.py
+++ dispatch_backend/apps.py
@@ -8,5 +8,6 @@ class BotConfig(AppConfig):
     name = 'dispatch_backend'
 
     def ready(self):
+        print("Startup hook for bot")
         if os.environ.get('RUN_MAIN'):
-            subprocess.Popen(["python", "bot_script.py"])
+            subprocess.Popen("python bot_script.py", shell=True)
