07c34af0d67085d444e35cf58d94475a7ef39f52
Felix Heeger <fheeger@posteo.de>
2024-06-04 20:29:55 +0200

    fix channel choice

--- dispatch_backend/forms.py
+++ dispatch_backend/forms.py
@@ -5,12 +5,11 @@ from dispatch_backend.models import Message, Channel
 
 class MessageForm(forms.ModelForm):
 
-    channels = forms.ModelMultipleChoiceField(queryset=Channel.objects.all(), required=False)
+    channels = forms.ModelMultipleChoiceField(queryset=Channel.objects.none(), required=False)
     turn_when_received = forms.IntegerField(required=False)
     is_lost = forms.BooleanField(required=False)
     approved = forms.BooleanField(required=False)
     version = forms.IntegerField(required=False)
-
 
     class Meta:
         model = Message
@@ -24,6 +23,7 @@ class MessageForm(forms.ModelForm):
             if "version" in self.fields:
                 self.fields["version"].widget = forms.HiddenInput()
                 self.fields['version'].initial = instance.version
+            self.fields['channels'].choices = [(channel.id, channel.name) for channel in Channel.objects.filter(game=instance.game)]
             self.fields['channels'].initial = [c.pk for c in instance.channels.all()]
             self.fields['turn_when_received'].initial = instance.turn_when_received
             self.fields['is_lost'].initial = instance.is_lost
